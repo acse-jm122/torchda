3075f6479d8fe25f9bdb32c229cfb43dd1be4ec0
acse-cd1f515d <jingyang.min22@imperial.ac.uk>
2023-06-28 01:17:50 +0100

    Add multi-dim x0 support

--- deepda/variational.py
+++ deepda/variational.py
@@ -13,6 +13,8 @@ def apply_3DVar(
     threshold: float = 1e-5,
     max_iterations: int = 1000,
     learning_rate: float = 1e-3,
+    single_xb: bool = True,
+    batch_first: bool = True,
     logging: bool = True,
 ) -> torch.Tensor:
     """
@@ -20,18 +22,31 @@ def apply_3DVar(
     """
     new_x0 = torch.nn.Parameter(xb.clone().detach())
 
-    trainer = torch.optim.Adam([new_x0], lr=learning_rate)
-    for n in range(max_iterations):
-        trainer.zero_grad(set_to_none=True)
-        new_x0_minus_xb = new_x0 - xb
-        y_minus_H_new_x0 = y - H(new_x0)
-        loss = (
+    def J(xp: torch.Tensor, xb: torch.Tensor, y: torch.Tensor):
+        new_x0_minus_xb = xp - xb
+        y_minus_H_new_x0 = y - H(xp)
+        return (
             new_x0_minus_xb.reshape((1, -1)) @
             torch.linalg.solve(B, new_x0_minus_xb).reshape((-1, 1))
             + y_minus_H_new_x0.reshape((1, -1)) @
             torch.linalg.solve(R, y_minus_H_new_x0).reshape((-1, 1))
         )
-        loss.backward()
+
+    trainer = torch.optim.Adam([new_x0], lr=learning_rate)
+    for n in range(max_iterations):
+        trainer.zero_grad(set_to_none=True)
+        if single_xb:
+            loss = J(xb, y)
+        else:
+            loss = 0
+            sequence_length = xb.size(1) if batch_first else xb.size(0)
+            for i in range(sequence_length):
+                one_xp, one_xb = (
+                    (new_x0[:, i], xb[:, i]) if batch_first
+                    else (new_x0[i], xb[i])
+                )
+                loss += J(one_xp.ravel(), one_xb.ravel(), y[i])
+        loss.backward(retain_graph=True)
         grad_norm = torch.norm(new_x0.grad)
         if logging:
             print(f"Iterations: {n}, Norm of J gradient: {grad_norm.item()}")
@@ -56,6 +71,8 @@ def apply_4DVar(
     threshold: float = 1e-5,
     max_iterations: int = 1000,
     learning_rate: float = 1e-3,
+    single_xb: bool = True,
+    batch_first: bool = True,
     logging: bool = True,
 ) -> torch.Tensor:
     """
@@ -63,25 +80,43 @@ def apply_4DVar(
     """
     new_x0 = torch.nn.Parameter(xb.clone().detach())
 
+    def Jb(xp: torch.Tensor, xb: torch.Tensor):
+        xp_minus_xb = xp - xb
+        return (
+            xp_minus_xb.reshape((1, -1)) @
+            torch.linalg.solve(B, xp_minus_xb).reshape((-1, 1))
+        )
+
+    def Jo(xp: torch.Tensor, y: torch.Tensor):
+        y_minus_H_xb = y - H(xp)
+        return (
+            y_minus_H_xb.reshape((1, -1)) @
+            torch.linalg.solve(R, y_minus_H_xb).reshape((-1, 1))
+        )
+
     trainer = torch.optim.Adam([new_x0], lr=learning_rate)
     for n in range(max_iterations):
         trainer.zero_grad(set_to_none=True)
         current_time = 0
         xp = new_x0
-        xp_minus_xb = xp - xb
-        total_loss = (xp_minus_xb.reshape((1, -1)) @
-                      torch.linalg.solve(B, xp_minus_xb).reshape((-1, 1)))
+        if single_xb:
+            total_loss = Jb(xp, xb)
+        else:
+            total_loss = 0
+            sequence_length = xb.size(1) if batch_first else xb.size(0)
+            for i in range(sequence_length):
+                one_xp, one_xb = (
+                    (xp[:, i], xb[:, i]) if batch_first
+                    else (xp[i], xb[i])
+                )
+                total_loss += Jb(one_xp.ravel(), one_xb.ravel())
         for iobs in range(nobs + 1):
             time_fw = torch.linspace(
                 current_time, time_obs[iobs], gap + 1, device=xb.device
             )
             xf = M(xp, time_fw, *model_args)
             xp = xf[:, -1]
-            y_minus_H_xb = y[:, iobs] - H(xp)
-            total_loss += (
-                y_minus_H_xb.reshape((1, -1)) @
-                torch.linalg.solve(R, y_minus_H_xb).reshape((-1, 1))
-            )
+            total_loss += Jo(xp, y[:, iobs])
             current_time = time_obs[iobs]
         total_loss.backward()
         grad_norm = torch.norm(new_x0.grad)
